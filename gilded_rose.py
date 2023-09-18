7544a6b3b8114a14e0ae20b4c2234612170283d8
kipariz <denys.duvinskyi@gmail.com>
2023-09-18 18:56:04 +0300

    my attempt to rewrite uodate method based on item name

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,44 +8,38 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             new_quality = item.quality
-            new_sell_in = item.sell_in
+            new_sell_in = item.sell_in - 1
 
             if item.name == "Aged Brie":
+                if new_sell_in < 0 and new_quality < 50:
+                    new_quality = new_quality + 1
                 if new_quality < 50:
                     new_quality = new_quality + 1
-            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if new_quality < 50:
-                    new_quality = new_quality + 1
-                    if new_quality < 50:
-                        if new_sell_in < 11:
-                            new_quality = new_quality + 1
-                        if new_sell_in < 6:
-                            new_quality = new_quality + 1
-            else:
-                if new_quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        new_quality = new_quality - 1
 
-            if item.name == "Sulfuras, Hand of Ragnaros":
-                item.quality = new_quality
-                item.sell_in = new_sell_in
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if new_sell_in < 0:
+                    new_quality = 0
+                    continue
+
+                if new_quality < 50:
+                    if new_sell_in <= 5:
+                        new_quality = new_quality + 3
+                    elif new_sell_in <= 10:
+                        new_quality = new_quality + 2
+                    else:
+                        new_quality = new_quality + 1
+
+            elif item.name == "Sulfuras, Hand of Ragnaros":
                 continue
             else:
-                new_sell_in = new_sell_in - 1
+                if new_sell_in < 0 and new_quality > 0:
+                    new_quality = new_quality - 1
+                if new_quality > 0:
+                    new_quality = new_quality - 1
+                
 
-            if new_sell_in < 0:
-                if item.name == "Aged Brie":
-                    if new_quality < 50:
-                        new_quality = new_quality + 1
-                else:
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        new_quality = 0
-                    else:
-                        if new_quality > 0:
-                            new_quality = new_quality - 1
-        
-        item.quality = new_quality
-        item.sell_in = new_sell_in
+            item.quality = new_quality
+            item.sell_in = new_sell_in
 
 
 class Item:
